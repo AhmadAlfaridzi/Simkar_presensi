2c9cb365ba2151c13229c6ed1d3d0c52d2f78b6e
Ahmad Alfaridzi <Ahmadalfaridzi5@gmail.com>
2025-08-01 14:57:10 +0700

    testing

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -11,8 +11,25 @@ export async function POST(request: Request) {
     }
 
     const user = await prisma.user.findUnique({
-      where: { username: username.trim() }
+      where: { username: username.trim() },
+      select: {
+        id: true,
+        username: true,
+        role: true,
+        passwordHash: true,
+        status: true,
+        name: true,
+        email: true,
+        position: true,
+        department: true,
+        image: true,
+      },
     })
+
+      console.log('📦 USER DATA:', user)
+    if (user) {
+      console.log('📌 passwordHash:', user.passwordHash)
+    }
 
     if (!user) {
       await bcrypt.compare(password, '$2a$10$fakehashforprotection')
@@ -45,8 +62,4 @@ export async function POST(request: Request) {
     console.error('🔴 LOGIN ERROR:', error)
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
   }
-}
-console.log('📦 USER DATA:', user)
-if (user) {
-  console.log('📌 passwordHash:', user.passwordHash)
 }
