5ed88127d3c65ff8ca7f5eb69df30ff6708b0737
Ahmad Alfaridzi <Ahmadalfaridzi5@gmail.com>
2025-08-07 10:39:30 +0700

    record

--- src/types/attendance.d.ts
+++ src/types/attendance.d.ts
@@ -1,26 +1,21 @@
 import type { AttendanceStatus } from './user';
 
-export interface Employee {
-  id: string;
-  name: string;
-  department: string;
-  position: string;
-}
-
 export interface AttendanceRecord {
-  id: string;
+  id_at: string;
+  userId: string;
   date: string;
   clockIn: string;
   clockOut?: string | null;
   status: AttendanceStatus;
-  employee: Employee;
+
   photoIn?: string | null;
   photoOut?: string | null;
+  latitude?: number | null;
+  longitude?: number | null;
+  location?: string | null;
   barcodeIn?: string | null;
   barcodeOut?: string | null;
   barcodeInAt?: string | null;
   barcodeOutAt?: string | null;
-  latitude?: number | null;
-  longitude?: number | null;
-  location?: string | null;
+  createdAt?: string | null;
 }
