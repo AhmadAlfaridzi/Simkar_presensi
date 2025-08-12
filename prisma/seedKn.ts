689e3011004c3e1a1b2884ccbc09ccf5b850d254
Ahmad Alfaridzi <Ahmadalfaridzi5@gmail.com>
2025-08-12 16:34:55 +0700

    seed

--- prisma/seedKn.ts
+++ prisma/seedKn.ts
@@ -3,10 +3,11 @@ import { prisma } from '../src/lib/prisma'
 async function main() {
   await prisma.kantor.create({
     data: {
-      nama: 'Apotek Aishy',
-      alamat: 'Jl. Banda Aceh - Medan No.07, Mns Mee, Kec. Muara Dua, Kota Lhokseumawe, Aceh 24355',
-      latitude: 5.166254982245708, 
-      longitude: 97.13566795922469,
+      kodeKantor:'KTR-001',
+      nama: 'PT. AISHY AND SONS',
+      alamat: 'Jl. Darussalam No.18, Kp. Jawa Baru, Kec. Banda Sakti, Kota Lhokseumawe, Aceh',
+      latitude: 5.1862466081887435, 
+      longitude:  97.1464353751523,
       radiusMeter: 20, 
     },
   })
